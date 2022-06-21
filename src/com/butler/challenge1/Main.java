cbbbeb327d43cabdc0e31b44103cb1d5bce96532
Jake Butler <JakeButler.student@careerdevs.com>
2022-06-21 19:31:18 -0400

    Added challenge 1

---
+++ src/com/butler/challenge1/Main.java
@@ -0,0 +1,10 @@
+package com.butler.challenge1;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Jake";
+        String lastName = "Butler";
+        String fullName = firstName + ' ' + lastName;
+        System.out.println(fullName);
+    }
+}
