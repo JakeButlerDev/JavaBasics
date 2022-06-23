7d71327bc9dab45cb146ef0945e255a29c8fb5d7
Jake Butler <JakeButler.student@careerdevs.com>
2022-06-23 17:25:29 -0400

    Added Car Class challenge, using getters and setters

---
+++ src/com/butler/challenge2Car/Car.java
@@ -0,0 +1,47 @@
+package com.butler.challenge2Car;
+
+public class Car  {
+    // Initialize fields
+    private String make;
+    private String model;
+    private int mileage;
+    private float gasTankPercent;
+
+//    public Car(String make, String model, int mileage, float gasTankPercent) {
+//        this.make = make;
+//        this.model = model;
+//        this.mileage = mileage;
+//        this.gasTankPercent = gasTankPercent;
+//    }
+    // BONUS
+    // Define Getters
+    public String getMake() {
+        return make;
+    }
+    public String getModel() {
+        return model;
+    }
+    public Integer getMileage() { return mileage;}
+    public Float getGasTankPercent() { return gasTankPercent;}
+
+    // Define Setters
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+    public void setGasTankPercent(float gasTankPercent) {
+        this.gasTankPercent = gasTankPercent;
+    }
+
+    public void toCarString() {
+        System.out.println("Make:" + getMake() + " " + "Model:" + getModel() + " " + "mileage:" + getMileage() + " " + "gasTankPercent:" + getGasTankPercent());
+    }
+
+
+}
---
+++ src/com/butler/challenge2Car/Main.java
@@ -0,0 +1,38 @@
+package com.butler.challenge2Car;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Car car1 = new Car("Camaro 2SS", "Chevy", 10000, 0.10F);
+//        Car car2 = new Car("Mustang GT", "Ford", 30000, 0.50F);
+//        Car car3 = new Car("Hellcat", "Dodge", 100, 0.90F);
+
+//        System.out.println(car1);
+//        System.out.println(car2);
+//        System.out.println(car3);
+
+        // BONUS
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+
+        car1.setMake("Camaro2SS");
+        car1.setModel("Chevy");
+        car1.setMileage(10000);
+        car1.setGasTankPercent(0.10F);
+
+        car2.setMake("Mustang GT");
+        car2.setModel("Ford");
+        car2.setMileage(30000);
+        car2.setGasTankPercent(0.50F);
+
+        car3.setMake("Hellcat");
+        car3.setModel("Dodge");
+        car3.setMileage(100);
+        car3.setGasTankPercent(0.90F);
+
+        car1.toCarString();
+        car2.toCarString();
+        car3.toCarString();
+    }
+}
